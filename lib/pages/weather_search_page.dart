17f9d71ead1b348c4af6e4c486e8391161aefae3
Yusron Hanan <yusronzain@gmail.com>
2021-11-02 22:12:37 +0700

    finished project - cubit

---
+++ lib/pages/weather_search_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
+import 'package:flutter_cubit_bloc_tutorial/data/model/weather.dart';
+
+class WeatherSearchPage extends StatefulWidget {
+  @override
+  _WeatherSearchPageState createState() => _WeatherSearchPageState();
+}
+
+class _WeatherSearchPageState extends State<WeatherSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Weather Search"),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        alignment: Alignment.center,
+        // TODO: Implement with cubit
+        child: BlocConsumer<WeatherCubit, WeatherState>(
+          //combined of BlocListener and BlocBuilder
+          listener: (context, state) {
+            if (state is WeatherError) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(state.message)));
+            }
+          },
+          builder: (context, state) {
+            if (state is WeatherInitial) {
+              return buildInitialInput();
+            } else if (state is WeatherLoading) {
+              return buildLoading();
+            } else if (state is WeatherLoaded) {
+              return buildColumnWithData(state.weather);
+            } else {
+              //state is WeatherError
+              return buildInitialInput();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildInitialInput() {
+    return Center(
+      child: CityInputField(),
+    );
+  }
+
+  Widget buildLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Column buildColumnWithData(Weather weather) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          weather.cityName,
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Text(
+          // Display the temperature with 1 decimal place
+          "${weather.temperatureCelsius.toStringAsFixed(1)} °C",
+          style: TextStyle(fontSize: 80),
+        ),
+        CityInputField(),
+      ],
+    );
+  }
+}
+
+class CityInputField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: TextField(
+        onSubmitted: (value) => submitCityName(context, value),
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          hintText: "Enter a city",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          suffixIcon: Icon(Icons.search),
+        ),
+      ),
+    );
+  }
+
+  void submitCityName(BuildContext context, String cityName) {
+    // TODO: Get weather for the city
+    /*
+    final weatherBloc = context.bloc()
+    context.bloc() is now deprecated, but that was getting the existing bloc created by the BlocConsumer widget. The reason we use the context is so that we can traverse the Widget tree and find the closest defined bloc of type WeatherBloc
+
+    The new pattern uses context.read() to get the value once, or context.watch() to subscribe to changes in the value.
+
+    I replaced the line above with:
+    final weatherBloc = context.read()
+    */
+    final weatherCubit = context.read<WeatherCubit>();
+    weatherCubit.getWeather(cityName);
+  }
+}
